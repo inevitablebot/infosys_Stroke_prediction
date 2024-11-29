471d1f4ed35aac4686117c789ab71a99c13dcbc6
inevitablebot <arnavanshul5@gmail.com>
2024-11-29 17:21:42 +0530

    update

--- main/custom_function.py
+++ main/custom_function.py
@@ -5,7 +5,6 @@ import seaborn as sns
 import warnings
 warnings.filterwarnings('ignore')
 import datetime as dt
-# from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import Ridge, Lasso, LinearRegression
